cb8d4f1192c285213dd189c0d4beddf144cdfa78
936167889 <152575309+936167889@users.noreply.github.com>
2026-06-30 17:21:01 +0800

    Update build.gradle.kts

--- VoicePingAndroidOfflineTranscribe/app/build.gradle.kts
+++ VoicePingAndroidOfflineTranscribe/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
-            abiFilters += listOf("arm64-v8a", "x86_64")
+            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64", "x86")
         }
     }
 
